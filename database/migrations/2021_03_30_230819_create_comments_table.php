0f0d9bf88158b80fe83cc6f0517e8642885f7f83
mxviz <vetal1423@gmail.com>
2021-03-31 02:24:04 +0300

    made migrations

---
+++ database/migrations/2021_03_30_230819_create_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('company_id');
+            $table->unsignedBigInteger('creator_id');
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');;
+            $table->foreign('creator_id')->references('id')->on('companies')->onDelete('cascade');;
+            $table->text('comment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
